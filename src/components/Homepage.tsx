d6935d813e0f1d4d56c59401f1e236622899e2e7
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2026-08-29 00:03:08 +0200

    refactoring

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -2,12 +2,12 @@ import { LuShoppingBasket } from "react-icons/lu";
 
 const Homepage = () => {
       return (
-            <div className="border border-red-700 h-full w-full p-4 grid grid-cols-2 mg:grid-cols-1">
+            <div className="border border-red-700 h-full w-full p-4 grid grid-cols-2 mg :grid-cols-1">
                   <section className="border border-amber-900 w-[50%] ">
-                        <p className="flex items-center gap-3">
+                        <div className="flex items-center gap-3">
                               <LuShoppingBasket />
                               <h2>Mi lista de compras</h2>
-                        </p>
+                        </div>
 
                   </section>
                   <section className="border border-amber-900 w-full">
