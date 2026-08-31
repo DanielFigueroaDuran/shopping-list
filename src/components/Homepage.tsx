90050b7b01d1d567e0b451cefd96819d2ff983ab
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2026-08-31 22:50:30 +0200

    adding styles

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,18 +1,38 @@
 import { LuShoppingBasket } from "react-icons/lu";
+import { IoIosList } from "react-icons/io";
+import { BiCategory } from "react-icons/bi";
+import { MdHistory } from "react-icons/md";
 
 const Homepage = () => {
       return (
-            <div className="border border-red-700 h-full w-full p-4 grid grid-cols-2 mg :grid-cols-1">
-                  <section className="border border-amber-900 w-[50%] ">
-                        <div className="flex items-center gap-3">
-                              <LuShoppingBasket />
-                              <h2>Mi lista de compras</h2>
+            <div className="border border-red-700 h-full w-full p-6 grid grid-cols-1 lg:grid-cols-[30%_70%] justify-items-start">
+                  <aside className="">
+                        <div className="flex flex-col gap-4">
+                              <div className="flex gap-3 items-center text-shadow-lg font-bold">
+                                    <LuShoppingBasket className="text-green-600 text-2xl" />
+                                    <h2>Mi lista de compras</h2>
+                              </div>
+                              <nav>
+                                    <ul className="space-y-2">
+                                          <li className="flex items-center gap-2 hover:text-green-600 hover:bg-gray-100 hover:rounded-xs">
+                                                <IoIosList className="" />
+                                                <a href="#">Lista</a>
+                                          </li>
+                                          <li className="flex items-center gap-2 hover:text-green-600 hover:bg-gray-100 hover:rounded-xs">
+                                                <BiCategory />
+                                                <a href="#">Categorias</a>
+                                          </li>
+                                          <li className="flex items-center gap-2 hover:text-green-600 hover:bg-gray-100 hover:rounded-xs">
+                                                <MdHistory />
+                                                <a href="#">Historial</a>
+                                          </li>
+                                    </ul>
+                              </nav>
                         </div>
-
-                  </section>
-                  <section className="border border-amber-900 w-full">
-                        seccion2
-                  </section>
+                  </aside>
+                  <main className="border border-amber-900 w-full text-shadow-lg font-bold">
+                        <h2>Lista de Compras</h2>
+                  </main>
             </div>
       )
 }
